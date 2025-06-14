b1a71fb036f4e81e29d7979d8b8f0e665901c03d
Mac709 <justin.s.dje35284syeg@gmail.com>
2025-06-15 08:30:08 +0900

    fetcher_Update

--- yield_fetcher.py
+++ yield_fetcher.py
@@ -15,7 +15,7 @@ PLOT_FILE = "yield_plot.png"
 # データ取得（当日分）
 today = datetime.today()
 today_str = today.strftime('%Y-%m-%d')
-data = yf.download(TICKER, start=today_str, end=today_str)
+data = yf.download(TICKER, start=today_str, end=today_str, auto_adjust=False)
 
 if data.empty:
     print("No data available for today.")
